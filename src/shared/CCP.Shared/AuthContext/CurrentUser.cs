33d11ad3617cc71b9751002e21600845c1787b3c
Aksel Fabricius <akselfabricius@gmail.com>
2026-03-24 13:00:36 +0100

    Setup Shared project

---
+++ src/shared/CCP.Shared/AuthContext/CurrentUser.cs
@@ -0,0 +1,19 @@
+﻿namespace CCP.Shared.AuthContext
+{
+    public class CurrentUser : ICurrentUser
+    {
+        public Guid UserId { get; private set; }
+
+        public Guid OrganizationId { get; private set; }
+
+        public void SetCurrentUser(Guid userId)
+        {
+            UserId = userId;
+        }
+
+        public void SetOrganizationId(Guid organizationId)
+        {
+            OrganizationId = organizationId;
+        }
+    }
+}
---
+++ src/shared/CCP.Shared/AuthContext/ICurrentUser.cs
@@ -0,0 +1,11 @@
+﻿namespace CCP.Shared.AuthContext
+{
+    public interface ICurrentUser
+    {
+        Guid UserId { get; }
+        Guid OrganizationId { get; }
+
+        void SetCurrentUser(Guid userId);
+        void SetOrganizationId(Guid organizationId);
+    }
+}
