775040bb80e3f95cd468529a2dc97b83e61b27b2
Aksel Fabricius <akselfabricius@gmail.com>
2026-03-27 12:19:19 +0100

    .

--- src/ui/CCP.Website/Program.cs
+++ src/ui/CCP.Website/Program.cs
@@ -71,10 +71,11 @@ namespace CCP.Website
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
+
             }
 
             app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-            app.UseHttpsRedirection();
 
             app.UseAntiforgery();
 
