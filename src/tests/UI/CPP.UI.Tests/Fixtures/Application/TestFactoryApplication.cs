fc7e401e9b53c3b25cdd867aba064a7d3e5b5ab9
Aksel Fabricius <akselfabricius@gmail.com>
2026-04-14 12:52:03 +0200

    .

---
+++ src/tests/UI/CPP.UI.Tests/Fixtures/Application/TestFactoryApplication.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+namespace CPP.UI.Tests.Fixtures.Application
+{
+    public class TestFactoryApplication : WebApplicationFactory<CCP.UI.Program>
+    {
+        public string BaseUrl { get; private set; } = default!;
+
+        private readonly Dictionary<System.Type, object> _mockproviders = new();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Development"); // Set the environment to Test
+            builder.UseConfiguration(new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    ["services:Keycloak:http:0"] = "http://localhost:8080",
+                    ["services:ccp-ui:https:0"] = "http://localhost:5000",
+                    ["services:identityservice-api:https:0"] = "http://localhost:5001",
+                    ["CircuitOptions.DetailedErrors"] = "true",
+                })
+                .Build());
+            builder.ConfigureServices(services =>
+            {
+                services.AddFluentUIComponents();
+
+                ConfigureMocks(services);
+                ConfigureAuth(services);
+            });
+        }
+
+        private void ConfigureMocks(IServiceCollection services)
+        {
+
+        }
+
+        private void AddMockedScoped<T>(IServiceCollection services)
+            where T : class
+        {
+            RemoveService<T>(services);
+
+            var provider = new MockProvider<T>();
+            _mockproviders[typeof(T)] = provider;
+
+            services.AddSingleton(provider);
+
+            services.AddScoped(sp =>
+            {
+                var mock = provider.Current;
+
+                if (mock == null)
+                    throw new InvalidOperationException(
+                        $"Mock for {typeof(T).Name} not configured.");
+
+                return mock;
+            });
+        }
+
+        private void RemoveService<T>(IServiceCollection services)
+        {
+            var descriptor = services.FirstOrDefault(
+                d => d.ServiceType == typeof(T));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+        }
+
+        public T SetMock<T>() where T : class
+        {
+            var mock = NSubstitute.Substitute.For<T>();
+            ((MockProvider<T>)_mockproviders[typeof(T)]).Current = mock;
+            return mock;
+        }
+
+        public void ResetMocks()
+        {
+            foreach (var provider in _mockproviders.Values)
+            {
+                ((dynamic)provider).ClearReceivedCalls();
+            }
+        }
+
+        // -------------------------
+        // Auth
+        // -------------------------
+        private void ConfigureAuth(IServiceCollection services)
+        {
+            services.AddAuthorizationCore();
+
+            services.AddScoped<
+                Microsoft.AspNetCore.Components.Authorization.AuthenticationStateProvider,
+                FakeAuthStateProvider>();
+        }
+    }
+}
---
+++ src/tests/UI/CPP.UI.Tests/Utils/Setup.cs
@@ -0,0 +1,54 @@
+﻿using CPP.UI.Tests.Fixtures.Website;
+
+namespace CPP.UI.Tests.Utils
+{
+    public class Setup
+    {
+        private void AddMockedScoped<T>(IServiceCollection services)
+          where T : class
+        {
+            RemoveService<T>(services);
+
+            var provider = new MockProvider<T>();
+            _mockproviders[typeof(T)] = provider;
+
+            services.AddSingleton(provider);
+
+            services.AddScoped(sp =>
+            {
+                var mock = provider.Current;
+
+                if (mock == null)
+                    throw new InvalidOperationException(
+                        $"Mock for {typeof(T).Name} not configured.");
+
+                return mock;
+            });
+        }
+
+        private void RemoveService<T>(IServiceCollection services)
+        {
+            var descriptor = services.FirstOrDefault(
+                d => d.ServiceType == typeof(T));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+        }
+
+        public T SetMock<T>() where T : class
+        {
+            var mock = NSubstitute.Substitute.For<T>();
+            ((MockProvider<T>)_mockproviders[typeof(T)]).Current = mock;
+            return mock;
+        }
+
+        public void ResetMocks()
+        {
+            foreach (var provider in _mockproviders.Values)
+            {
+                ((dynamic)provider).ClearReceivedCalls();
+            }
+        }
+
+    }
+}
