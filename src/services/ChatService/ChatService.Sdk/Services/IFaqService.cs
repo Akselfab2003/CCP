d26f698d41a4144e327b6f9aa42555d3e4845842
Aksel Fabricius <akselfabricius@gmail.com>
2026-04-19 12:33:49 +0200

    .

--- src/services/ChatService/ChatService.Sdk/Services/IFaqService.cs
+++ src/services/ChatService/ChatService.Sdk/Services/IFaqService.cs
@@ -1,9 +1,14 @@
 ﻿using CCP.Shared.ResultAbstraction;
+using ChatService.Sdk.Models;
 
 namespace ChatService.Sdk.Services
 {
     internal interface IFaqService
     {
         Task<Result> CreateNewFaqEntry(string question, string answer);
+        Task<Result> DeleteFaq(int id);
+        Task<Result<List<FaqModel>>> GetAllFaqEntries();
+        Task<Result<List<FaqModel>>> SearchFaqEntries(string query);
+        Task<Result> UpdateFaq(int id, string question, string answer, string category);
     }
 }
