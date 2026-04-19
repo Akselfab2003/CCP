bee962ee87c747b36b055ceb028dfb79c204bdb1
Aksel Fabricius <akselfabricius@gmail.com>
2026-04-19 16:16:20 +0200

    .

--- src/ui/CCP.UI/Pages/FaqConfiguration/FaqConfigurationPage.razor.cs
+++ src/ui/CCP.UI/Pages/FaqConfiguration/FaqConfigurationPage.razor.cs
@@ -15,8 +15,15 @@ namespace CCP.UI.Pages.FaqConfiguration
 
         private readonly List<FaqModel> _faqEntries = [];
         private int ExpandedFaqId = 0;
-        //private bool IsCreatingNewEntry = false;
+        private bool IsCreatingNewEntry = false;
+        private FaqModel? NewFaqEntry { get; set; }
 
+        private void StartCreateFaq()
+        {
+            NewFaqEntry = new FaqModel();
+            IsCreatingNewEntry = true;
+            ExpandedFaqId = 0;
+        }
         protected override async Task OnInitializedAsync()
         {
             var result = await _faqService.GetAllFaqEntries();
@@ -44,21 +51,70 @@ namespace CCP.UI.Pages.FaqConfiguration
 
         private async Task DeleteFaqEntry(int faqId)
         {
+            try
+            {
+                var result = await _faqService.DeleteFaq(faqId);
+                if (result.IsSuccess)
+                    await ClearAndReload();
+            }
+            catch (Exception)
+            {
+            }
 
 
+        }
+        private async Task SaveNewFaqEntry()
+        {
+            if (NewFaqEntry is not null)
+            {
+                var result = await _faqService.CreateNewFaqEntry(NewFaqEntry.Question, NewFaqEntry.Answer);
+                if (result.IsSuccess)
+                {
+                    await ClearAndReload();
+                }
+            }
         }
 
         private async Task SaveFaqEntry(FaqModel faqEntry)
         {
+            try
+            {
+                var update = await _faqService.UpdateFaq(faqEntry.Id, faqEntry.Question, faqEntry.Answer, faqEntry.Category!);
+                if (update.IsSuccess)
+                {
+                    await ClearAndReload();
+                }
+            }
+            catch (Exception)
+            {
+
+
+            }
         }
 
-        private async Task CreateFaqEntry()
-        {
-        }
-
-        private async Task CancelEdit()
+        private async Task CancelCreateFaq()
         {
             ExpandedFaqId = 0;
+            IsCreatingNewEntry = false;
+            NewFaqEntry = null;
+        }
+        private async Task CancelEdit()
+        {
+            await ClearAndReload();
+            ExpandedFaqId = 0;
+        }
+
+        private async Task ClearAndReload()
+        {
+            // Reload the list
+            _faqEntries.Clear();
+            var reload = await _faqService.GetAllFaqEntries();
+            if (reload.IsSuccess)
+                _faqEntries.AddRange(reload.Value);
+            IsCreatingNewEntry = false;
+            NewFaqEntry = null;
+
+            StateHasChanged();
         }
     }
 }
