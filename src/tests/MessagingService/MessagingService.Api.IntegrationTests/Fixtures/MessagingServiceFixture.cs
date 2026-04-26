8b689c37f117ccfcb26c03a7d759d6c2d99be758
Aksel Fabricius <akselfabricius@gmail.com>
2026-04-26 17:59:26 +0200

    fix test issues

--- src/tests/MessagingService/MessagingService.Api.IntegrationTests/Fixtures/MessagingServiceFixture.cs
+++ src/tests/MessagingService/MessagingService.Api.IntegrationTests/Fixtures/MessagingServiceFixture.cs
@@ -1,8 +1,13 @@
-﻿using MessagingService.Application.Services;
+﻿using CCP.Shared.AuthContext;
+using Duende.AccessTokenManagement;
+using Duende.IdentityModel.Client;
+using MessagingService.Application.Services;
 using MessagingService.Domain.Interfaces;
 using MessagingService.Infrastructure.Persistence;
 using MessagingService.Sdk.ServiceDefaults;
+using Microsoft.Extensions.Configuration;
 using TestUtils.Integration;
+using TicketService.Sdk.ServiceDefaults;
 
 namespace MessagingService.Api.IntegrationTests.Fixtures
 {
@@ -12,14 +17,45 @@ namespace MessagingService.Api.IntegrationTests.Fixtures
         public override string DBResourceName => "MessagingDatabase";
         public override List<string> RequiredResources => [APIResourceName, DBResourceName, "keycloak", "postgres", "MailHog", "RabbitMQ", "ticketservice-api", "ticketdb", "emailservice-api", "emaildb", "customerdb", "customerservice-api"];
 
+        private IServiceCollection TicketSdk = null!;
+        public IServiceProvider TicketSDK = null!;
+
         public async ValueTask InitializeAsync()
         {
             await Initialize();
             DB_Services.AddScoped<IMessageService, MessageService>();
             var serviceUrl = GetServiceUrl(APIResourceName);
             SDK_Services.AddHttpContextAccessor();
+            SDK_Services.AddSingleton<ServiceAccountOverrider>();
             SDK_Services.AddMessageServiceSDK(serviceUrl, true);
+            TicketSdk = new ServiceCollection();
+            await InitializeSDK();
+            TicketSDK = TicketSdk
+                           .AddTicketServiceSdk(GetServiceUrl("ticketservice-api"), true)
+                           .AddSingleton<ServiceAccountOverrider>()
+                           .BuildServiceProvider();
+
             await BuildProviders();
+        }
+        private async Task InitializeSDK()
+        {
+            IConfiguration configuration = GetConfiguration();
+
+            TicketSdk.AddLogging();
+
+            // Configure OAuth2 client credentials for authentication
+            TicketSdk.AddClientCredentialsTokenManagement()
+                        .AddClient(ClientCredentialsClientName.Parse("CCP.ServiceAccount"), client =>
+                        {
+                            client.TokenEndpoint = new Uri("http://localhost:8080/realms/CCP/protocol/openid-connect/token");
+                            client.ClientId = ClientId.Parse("CCP.ServiceAccount");
+                            client.ClientSecret = ClientSecret.Parse(
+                                configuration["SERVICE_ACCOUNT_SECRET"]
+                                ?? throw new InvalidOperationException("SERVICE_ACCOUNT_SECRET configuration value is required.")
+                            );
+                            client.Scope = Scope.ParseOrDefault("openid");
+                            client.ClientCredentialStyle = ClientCredentialStyle.AuthorizationHeader;
+                        });
         }
 
         public async ValueTask DisposeAsync()
--- src/tests/MessagingService/MessagingService.Api.IntegrationTests/Tests/MessagesEndpointTests.cs
+++ src/tests/MessagingService/MessagingService.Api.IntegrationTests/Tests/MessagesEndpointTests.cs
@@ -1,6 +1,8 @@
-﻿using MessagingService.Api.IntegrationTests.Fixtures;
+﻿using CCP.Shared.AuthContext;
+using MessagingService.Api.IntegrationTests.Fixtures;
 using MessagingService.Sdk.Dtos;
 using MessagingService.Sdk.Services;
+using TicketService.Sdk.Services.Ticket;
 
 namespace MessagingService.Api.IntegrationTests.Tests
 {
@@ -18,16 +20,36 @@ namespace MessagingService.Api.IntegrationTests.Tests
         public async Task CreateMessage_ShouldCreateMessageSuccessfully()
         {
             // Arrange
+            Guid Org_Id = Guid.NewGuid();
             using var SDK_Scope = _fixture.SDK.CreateScope();
             using var DB_Scope = _fixture.DB.CreateScope();
+            using var ticketScope = _fixture.TicketSDK.CreateScope();
             IMessageSdkService messageSdkService = SDK_Scope.ServiceProvider.GetRequiredService<IMessageSdkService>();
-            Guid Org_Id = Guid.NewGuid();
+            ITicketService ticketService = ticketScope.ServiceProvider.GetRequiredService<ITicketService>();
+            ticketScope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+            SDK_Scope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+
             Guid User_Id = Guid.NewGuid();
-            int TicketId = Random.Shared.Next(1, 1000);
+            var ticketResult = await ticketService.CreateTicket(new TicketService.Sdk.Dtos.CreateTicketRequestDto()
+            {
+
+                Title = "Test",
+                AssignedUserId = User_Id,
+                CustomerId = Guid.NewGuid(),
+                Description = "Test",
+                OrganizationId = Org_Id
+            }, CCP.Shared.ValueObjects.TicketOrigin.Manual, TestContext.Current.CancellationToken);
+
+            Assert.NotNull(ticketResult);
+            Assert.True(ticketResult.IsSuccess);
+
+
+            var ticketId = ticketResult.Value;
+
 
 
             // Act
-            Result<MessageDto> result = await messageSdkService.CreateMessageAsync(ticketId: TicketId,
+            Result<MessageDto> result = await messageSdkService.CreateMessageAsync(ticketId: ticketId,
                                                                     organizationId: Org_Id,
                                                                     userId: User_Id,
                                                                     content: "Hello, this is a test message!",
@@ -35,7 +57,7 @@ namespace MessagingService.Api.IntegrationTests.Tests
             // Assert
             Assert.NotNull(result);
             Assert.True(result.IsSuccess);
-            Assert.Equal(TicketId, result.Value.TicketId);
+            Assert.Equal(ticketId, result.Value.TicketId);
             Assert.Equal(Org_Id, result.Value.OrganizationId);
             Assert.Equal(User_Id, result.Value.UserId);
             Assert.Equal("Hello, this is a test message!", result.Value.Content);
@@ -68,12 +90,26 @@ namespace MessagingService.Api.IntegrationTests.Tests
         public async Task GetMessageById_ShouldReturnMessageForValidId()
         {
             // Arrange
+            Guid Org_Id = Guid.NewGuid();
             using var SDK_Scope = _fixture.SDK.CreateScope();
             using var DB_Scope = _fixture.DB.CreateScope();
+            using var ticketScope = _fixture.TicketSDK.CreateScope();
             IMessageSdkService messageSdkService = SDK_Scope.ServiceProvider.GetRequiredService<IMessageSdkService>();
-            Guid Org_Id = Guid.NewGuid();
+            ITicketService ticketService = ticketScope.ServiceProvider.GetRequiredService<ITicketService>();
+            ticketScope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+            SDK_Scope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+
             Guid User_Id = Guid.NewGuid();
-            int TicketId = Random.Shared.Next(1, 1000);
+            var ticketResult = await ticketService.CreateTicket(new TicketService.Sdk.Dtos.CreateTicketRequestDto()
+            {
+
+                Title = "Test",
+                AssignedUserId = User_Id,
+                CustomerId = Guid.NewGuid(),
+                Description = "Test",
+                OrganizationId = Org_Id
+            }, CCP.Shared.ValueObjects.TicketOrigin.Manual, TestContext.Current.CancellationToken);
+            var TicketId = ticketResult.Value;
             Result<MessageDto> createResult = await messageSdkService.CreateMessageAsync(ticketId: TicketId,
                                                                           organizationId: Org_Id,
                                                                           userId: User_Id,
@@ -118,13 +154,26 @@ namespace MessagingService.Api.IntegrationTests.Tests
         public async Task GetMessagesByTicketId_ShouldReturnMessagesForGivenTicket()
         {
             // Arrange
+            Guid Org_Id = Guid.NewGuid();
             using var SDK_Scope = _fixture.SDK.CreateScope();
             using var DB_Scope = _fixture.DB.CreateScope();
+            using var ticketScope = _fixture.TicketSDK.CreateScope();
             IMessageSdkService messageSdkService = SDK_Scope.ServiceProvider.GetRequiredService<IMessageSdkService>();
-            Guid Org_Id = Guid.NewGuid();
+            ITicketService ticketService = ticketScope.ServiceProvider.GetRequiredService<ITicketService>();
+            ticketScope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+            SDK_Scope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+
             Guid User_Id = Guid.NewGuid();
-            int TicketId = Random.Shared.Next(1, 1000);
-            // Create a message to ensure there is at least one message for the ticket
+            var ticketResult = await ticketService.CreateTicket(new TicketService.Sdk.Dtos.CreateTicketRequestDto()
+            {
+
+                Title = "Test",
+                AssignedUserId = User_Id,
+                CustomerId = Guid.NewGuid(),
+                Description = "Test",
+                OrganizationId = Org_Id
+            }, CCP.Shared.ValueObjects.TicketOrigin.Manual, TestContext.Current.CancellationToken);
+            var TicketId = ticketResult.Value;
             await messageSdkService.CreateMessageAsync(ticketId: TicketId,
                                                         organizationId: Org_Id,
                                                         userId: User_Id,
@@ -163,13 +212,26 @@ namespace MessagingService.Api.IntegrationTests.Tests
         public async Task UpdateMessage_ShouldUpdateMessageSuccessfully()
         {
             // Arrange
+            Guid Org_Id = Guid.NewGuid();
             using var SDK_Scope = _fixture.SDK.CreateScope();
             using var DB_Scope = _fixture.DB.CreateScope();
+            using var ticketScope = _fixture.TicketSDK.CreateScope();
             IMessageSdkService messageSdkService = SDK_Scope.ServiceProvider.GetRequiredService<IMessageSdkService>();
-            Guid Org_Id = Guid.NewGuid();
-            Guid User_Id = Guid.NewGuid();
-            int TicketId = Random.Shared.Next(1, 1000);
+            ITicketService ticketService = ticketScope.ServiceProvider.GetRequiredService<ITicketService>();
+            ticketScope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+            SDK_Scope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
 
+            Guid User_Id = Guid.NewGuid();
+            var ticketResult = await ticketService.CreateTicket(new TicketService.Sdk.Dtos.CreateTicketRequestDto()
+            {
+
+                Title = "Test",
+                AssignedUserId = User_Id,
+                CustomerId = Guid.NewGuid(),
+                Description = "Test",
+                OrganizationId = Org_Id
+            }, CCP.Shared.ValueObjects.TicketOrigin.Manual, TestContext.Current.CancellationToken);
+            var TicketId = ticketResult.Value;
             Result<MessageDto> createResult = await messageSdkService.CreateMessageAsync(ticketId: TicketId,
                                                                           organizationId: Org_Id,
                                                                           userId: User_Id,
@@ -209,12 +271,26 @@ namespace MessagingService.Api.IntegrationTests.Tests
         public async Task DeleteMessage_ShouldSoftDeleteMessageSuccessfully()
         {
             // Arrange
+            Guid Org_Id = Guid.NewGuid();
             using var SDK_Scope = _fixture.SDK.CreateScope();
             using var DB_Scope = _fixture.DB.CreateScope();
+            using var ticketScope = _fixture.TicketSDK.CreateScope();
             IMessageSdkService messageSdkService = SDK_Scope.ServiceProvider.GetRequiredService<IMessageSdkService>();
-            Guid Org_Id = Guid.NewGuid();
+            ITicketService ticketService = ticketScope.ServiceProvider.GetRequiredService<ITicketService>();
+            ticketScope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+            SDK_Scope.ServiceProvider.GetRequiredService<ServiceAccountOverrider>().SetOrganizationId(Org_Id);
+
             Guid User_Id = Guid.NewGuid();
-            int TicketId = Random.Shared.Next(1, 1000);
+            var ticketResult = await ticketService.CreateTicket(new TicketService.Sdk.Dtos.CreateTicketRequestDto()
+            {
+
+                Title = "Test",
+                AssignedUserId = User_Id,
+                CustomerId = Guid.NewGuid(),
+                Description = "Test",
+                OrganizationId = Org_Id
+            }, CCP.Shared.ValueObjects.TicketOrigin.Manual, TestContext.Current.CancellationToken);
+            var TicketId = ticketResult.Value;
             Result<MessageDto> createResult = await messageSdkService.CreateMessageAsync(ticketId: TicketId,
                                                                           organizationId: Org_Id,
                                                                           userId: User_Id,
