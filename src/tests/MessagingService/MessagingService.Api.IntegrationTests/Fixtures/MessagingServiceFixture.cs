08fcc599f2b07fe81af22ebe69f15900023a3061
Aksel Fabricius <akselfabricius@gmail.com>
2026-04-27 18:34:18 +0200

    .

--- src/tests/MessagingService/MessagingService.Api.IntegrationTests/Fixtures/MessagingServiceFixture.cs
+++ src/tests/MessagingService/MessagingService.Api.IntegrationTests/Fixtures/MessagingServiceFixture.cs
@@ -15,13 +15,15 @@ namespace MessagingService.Api.IntegrationTests.Fixtures
     {
         public override string APIResourceName => "messagingservice-api";
         public override string DBResourceName => "MessagingDatabase";
-        public override List<string> RequiredResources => [APIResourceName, DBResourceName, "keycloak", "postgres", "MailHog", "RabbitMQ", "ticketservice-api", "ticketdb", "emailservice-api", "emaildb", "customerdb", "customerservice-api"];
+        public override List<string> RequiredResources => [APIResourceName, DBResourceName, "keycloak", "postgres", "MailHog", "RabbitMQ", "ticketservice-api", "ticketdb", "emailservice-api", "emaildb", "customerdb", "customerservice-api", "identityservice-api", "chatDB", "ollama", "embedding", "qwen"];
 
         private IServiceCollection TicketSdk = null!;
         public IServiceProvider TicketSDK = null!;
 
         public async ValueTask InitializeAsync()
         {
+            IsRemoveNotNeededResourcesForTestingEnabled = false;
+            DefaultTimeout = TimeSpan.FromMinutes(1);
             await Initialize();
             DB_Services.AddScoped<IMessageService, MessageService>();
             var serviceUrl = GetServiceUrl(APIResourceName);
